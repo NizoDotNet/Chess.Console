71bab5c700bb7e39640306610a349c42469ef22f
NizoDotNet <mmmdovnizami.official@gmail.com>
2024-04-08 21:33:11 +0400

    Queen movement

--- Chess/Pieces/Queen.cs
+++ Chess/Pieces/Queen.cs
@@ -1,18 +1,30 @@
 ﻿using Chess.Helpers;
 using Chess.Main;
+using Chess.Pieces.LongRangeMove;
+using Chess.Pieces.Move;
 
 namespace Chess.Pieces;
 
 public class Queen : Piece
 {
+    private readonly ILongRangeMove horizantal, vertical, diagonalLeft, diagonalRight;
     public Queen(Color color) : base(color)
     {
+        horizantal = new Horizantal();
+        vertical = new Vertical();
+        diagonalLeft = new DiagonalLeft();
+        diagonalRight = new DiagonalRight();    
+
     }
     public override string Name => nameof(Queen);
 
     public override List<Coordinate> GetAllowedMoves(Board board, Coordinate coordinate)
     {
-        throw new NotImplementedException();
+        
+        return [.. horizantal.Move(coordinate, board, Color), 
+            ..vertical.Move(coordinate, board, Color), 
+            ..diagonalLeft.Move(coordinate, board, Color), 
+            ..diagonalRight.Move(coordinate, board, Color)];
     }
 
 
