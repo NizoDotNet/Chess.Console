2ae08fe42e9563fc054578c0c04430d216607d02
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-11-10 23:24:31 +0400

    need write renderer

---
+++ Chess.Blazor/Application/BlazorRenderer.cs
@@ -0,0 +1,38 @@
+﻿using Chess.Helpers;
+using Chess.Main;
+using Chess.Pieces;
+
+namespace Chess.Blazor.Application;
+
+public class BlazorRenderer : IRenderer
+{
+    public Coordinate AskMove(Color color, Board board)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Error(string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void IsCheck()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Piece Promation(Color color)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Render(Board board)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Coordinate ShowAvaibleMoves(IList<Coordinate> coordinates)
+    {
+        throw new NotImplementedException();
+    }
+}
