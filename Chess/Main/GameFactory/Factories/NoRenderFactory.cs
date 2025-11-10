c0634dbd8859db2ddd64301fdadc5116658847c3
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-11-10 21:57:01 +0400

    pieces in board

---
+++ Chess/Main/GameFactory/Factories/NoRenderFactory.cs
@@ -0,0 +1,11 @@
+﻿using Chess.Main.GameFactory.Games;
+
+namespace Chess.Main.GameFactory.Factories;
+
+public class NoRenderFactory : IGameFactory
+{
+    public IGame CreateGame()
+    {
+        return new NoRenderer();
+    }
+}
