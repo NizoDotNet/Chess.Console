5bac5d0ba8e6f60319f46aa17b61720e4e0687f9
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-11-25 13:56:04 +0400

    bug with check

--- Chess.Blazor/Program.cs
+++ Chess.Blazor/Program.cs
@@ -9,6 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped(c => new ClientSideGame(null));
+//builder.Services.AddScoped(c => new ClientSideGame(null));
 
 await builder.Build().RunAsync();
