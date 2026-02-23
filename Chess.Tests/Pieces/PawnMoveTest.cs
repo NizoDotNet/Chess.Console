dcc6e823c0363e071c6bd35dc7f6d9b818836e12
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-02-23 14:27:54 +0400

    some test for pawn

---
+++ Chess.Tests/Pieces/PawnMoveTest.cs
@@ -0,0 +1,53 @@
+﻿using Chess.Main;
+
+namespace Chess.Tests.Pieces;
+
+public class PawnMoveTest
+{
+    [Fact]
+    public void Pawn_Should_Not_Jump_Over_Piece()
+    {
+        var board = new Board();
+        board.SetFen("4k3/8/8/8/8/5N2/PPPP1PPP/4K3/", null);
+        Helpers.Coordinate coordinate = new(2, Helpers.File.F);
+        var pawn = board.PieceAndCoordinates[coordinate];
+
+        // 
+        var moves = pawn.GetAllowedMoves(board, coordinate);
+
+        Assert.Empty(moves);
+    }
+
+    [Fact]
+    public void Pawn_Should_Return_2_Avaible_Moves()
+    {
+        var board = new Board();
+        board.SetFen("4k3/8/8/8/8/8/PPPP1PPP/4K3/", null);
+        Helpers.Coordinate coordinate = new(2, Helpers.File.F);
+        var pawn = board.PieceAndCoordinates[coordinate];
+
+        var moves = pawn.GetAllowedMoves(board, coordinate);
+
+        Assert.Equal(2, moves.Count);
+        Assert.Contains(moves, item => item.Rank == 3 && item.File == Helpers.File.F);
+        Assert.Contains(moves, item => item.Rank == 4 && item.File == Helpers.File.F);
+
+    }
+
+    [Fact]
+    public void Pawn_Should_Return_3_Avaible_Moves()
+    {
+        var board = new Board();
+        board.SetFen("4k3/8/8/8/8/4p3/PPPP1PPP/4K3/", null);
+        Helpers.Coordinate coordinate = new(2, Helpers.File.F);
+        var pawn = board.PieceAndCoordinates[coordinate];
+
+        var moves = pawn.GetAllowedMoves(board, coordinate);
+
+        Assert.Equal(3, moves.Count);
+        Assert.Contains(moves, item => item.Rank == 3 && item.File == Helpers.File.F);
+        Assert.Contains(moves, item => item.Rank == 4 && item.File == Helpers.File.F);
+        Assert.Contains(moves, item => item.Rank == 3 && item.File == Helpers.File.E);
+
+    }
+}
